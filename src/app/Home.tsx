0b0d062019b2cc1077979f010dac4942f16cd613
Blessed-Samuel <samuelalisigwe22@gmail.com>
2024-10-18 20:57:35 +0100

    made some modifications.

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <></>
+    <>
+      <section>
+        <h1>Amazing scientists</h1>
+        <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+        <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+        <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+      </section>
+    </>
   )
 }
 
